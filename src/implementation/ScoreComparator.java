2bd5b7ccb8ba418e9600d3f6f6667b9233f5c034
Momoroy <64591496+Momoroy@users.noreply.github.com>
2021-02-26 20:23:16 +0100

    Add files via upload

---
+++ src/implementation/ScoreComparator.java
@@ -0,0 +1,13 @@
+package src.implementation;
+
+import src.commons.*;
+
+import java.util.Comparator;
+
+public class ScoreComparator implements Comparator<Collaboration> {
+
+    public int compare(Collaboration m1, Collaboration m2) {
+        return m2.getScore().compareTo(m1.getScore());
+    }
+    
+}
